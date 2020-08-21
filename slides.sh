814458c14fc2b3383fc023dcafd1554e04fb4272
Hector Martinez-Seara <hseara@gmail.com>
2020-08-21 16:00:12 +0200

    add script to generate slides from courses

---
+++ slides.sh
@@ -0,0 +1,29 @@
+# Python-A1 course (PyIntro-A1)
+##############################
+jupyter nbconvert 00-PyIntro-Intro.ipynb --to slides
+jupyter nbconvert 01-PyIntro-Datatypes.ipynb --to slides
+jupyter nbconvert 02-PyIntro-Loops_Decisions.ipynb --to slides
+jupyter nbconvert 03-PyIntro-Syntax.ipynb --to slides
+jupyter nbconvert 04-PyIntro-functions.ipynb --to slides
+
+# Python-A2 course (PyIntro-A2)
+###############################
+
+
+# Python-B1 course (PyInter-B1)
+###############################
+jupyter nbconvert 10-PyInter-XXX.ipynb --to slides
+
+# Python-B2 course (PyInter-B2)
+###############################
+
+
+# Python-C1 course (PyAdv-C1)
+#############################
+
+
+# Python-C2 course (PyAdv-C2)
+#############################
+
+
+
