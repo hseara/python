55de7db64fc54486f750f7e3b9ab6c4a29bb8f7e
Hector Martinez-Seara <hseara@gmail.com>
2020-08-23 10:18:13 +0200

    slides.sh: Renamed notebooks to new nomenclature

--- slides.sh
+++ slides.sh
@@ -1,18 +1,19 @@
 # Python-A1 course (PyIntro-A1)
 ##############################
-jupyter nbconvert 00-PyIntro-Intro.ipynb --to slides
-jupyter nbconvert 01-PyIntro-Datatypes.ipynb --to slides
-jupyter nbconvert 02-PyIntro-Loops_Decisions.ipynb --to slides
-jupyter nbconvert 03-PyIntro-Syntax.ipynb --to slides
-jupyter nbconvert 04-PyIntro-functions.ipynb --to slides
+jupyter nbconvert a1-00-PyIntro-Intro.ipynb --to slides
+jupyter nbconvert a1-01-PyIntro-Datatypes.ipynb --to slides
+jupyter nbconvert a1-02-PyIntro-Loops_Decisions.ipynb --to slides
+jupyter nbconvert a1-03-PyIntro-Syntax.ipynb --to slides
+jupyter nbconvert a1-04-PyIntro-functions.ipynb --to slides
 
 # Python-A2 course (PyIntro-A2)
 ###############################
+jupyter nbconvert a2-01-PyIntro-IO.ipynb --to slides
 
 
 # Python-B1 course (PyInter-B1)
 ###############################
-jupyter nbconvert 10-PyInter-XXX.ipynb --to slides
+jupyter nbconvert b1-01-PyInter-XXX.ipynb --to slides
 
 # Python-B2 course (PyInter-B2)
 ###############################
